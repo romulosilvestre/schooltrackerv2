b272637dd6bc2851767b92277b957b3ffee647c9
Henrique Serafin <henriqueserafin8@gmail.com>
2024-08-21 08:19:53 -0300

    verificação de ra

--- main.py
+++ main.py
@@ -1,103 +1,98 @@
-from flask import Flask, render_template,request,redirect,url_for
-# Importa a função `sessionmaker`, que é usada para criar uma nova sessão para interagir com o banco de dados
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy.orm import sessionmaker
-
-# Importa as funções `create_engine` para estabelecer uma conexão com o banco de dados e `MetaData` para trabalhar com metadados do banco de dados
 from sqlalchemy import create_engine, MetaData
-
-# Importa a função `automap_base`, que é usada para refletir um banco de dados existente em classes ORM automaticamente
 from sqlalchemy.ext.automap import automap_base
-from aluno import Aluno
+import urllib.parse
 
 app = Flask(__name__)
 
-# Criando a configuração do banco de dados
 # Configuração do Banco de Dados
-# biblioteca para converter e resolver problema do @
-import urllib.parse
-
-# Qual o usuário do banco e a senha?
-
 user = 'root'
-password = urllib.parse.quote_plus('senai@123')
-
+password = urllib.parse.quote_plus('1234')
 host = 'localhost'
 database = 'schooltracker'
 connection_string = f'mysql+pymysql://{user}:{password}@{host}/{database}'
 
-# Criar a engine e refletir o banco de dados existente
 engine = create_engine(connection_string)
 metadata = MetaData()
 metadata.reflect(engine)
 
-# Mapeamento automático das tabelas para classes Python
 base = automap_base(metadata=metadata)
 base.prepare()
 
-# Acessando a tabela 'aluno' mapeada
 Aluno = base.classes.aluno
 
-
-
-# Criar a sessão do SQLAlchemy
 session = sessionmaker(bind=engine)
 session = session()
 
-
 @app.route("/")
 def index():
-    return render_template("index.html")
+    mensagem = request.args.get("mensagem", "")
+    return render_template("index.html", mensagem=mensagem)
+
+@app.route("/login", methods=["POST"])
+def login():
+    ra = request.form["ra"]
+    aluno = session.query(Aluno).filter_by(ra=ra).first()
+    
+    if aluno:
+        return redirect(url_for("listar_alunos"))
+    else:
+        mensagem = "RA não encontrado. Por favor, tente novamente."
+        return redirect(url_for("index", mensagem=mensagem))
 
 @app.route("/cadastro")
 def cadastro():
     return render_template("cadastro.html")
 
-@app.route('/novoaluno',methods=['POST'])
+@app.route('/novoaluno', methods=['POST'])
 def inserir_aluno():
     ra = request.form['ra']
     nome = request.form['nome']
     tempoestudo = request.form['tempoestudo']
     rendafamiliar = request.form['rendafamiliar']
-     
-    #sessão ok
-    aluno = Aluno(ra=ra,nome=nome,tempoestudo=tempoestudo,rendafamiliar=rendafamiliar)
+    
+    aluno = Aluno(ra=ra, nome=nome, tempoestudo=tempoestudo, rendafamiliar=rendafamiliar)
 
     try:
-       session.add(aluno)
-       session.commit() 
+        session.add(aluno)
+        session.commit()
+        mensagem = "Aluno cadastrado com sucesso."
     except:
-       session.rollback() 
-       raise
+        session.rollback()
+        mensagem = "Erro ao cadastrar o aluno."
     finally:
-       session.close()
-    mensagem = "cadastrado com sucesso"
-    return redirect(url_for('listar_alunos'))
+        session.close()
+    
+    return redirect(url_for('listar_alunos', mensagem=mensagem))
 
-@app.route('/alunos',methods=['GET'])
+@app.route('/alunos', methods=['GET'])
 def listar_alunos():
+    mensagem = request.args.get("mensagem", "")
     try:
-      #buscar todos os alunos do banco de dados
-      alunos = session.query(Aluno).all()
+        alunos = session.query(Aluno).all()
     except:
-      session.rollback()
-      msg = "erro ao tentar recuperar a lista de alunos"
-      return render_template('index.html',msgbanco=msg)  
+        session.rollback()
+        mensagem = "Erro ao tentar recuperar a lista de alunos"
+        return render_template('index.html', mensagem=mensagem)
     finally:
-      session.close()
+        session.close()
 
-    return render_template('listaalunos.html',alunos=alunos)
+    return render_template('listaalunos.html', alunos=alunos, mensagem=mensagem)
 
-@app.route("/remover_aluno/<int:id>",methods=["POST","GET"])
+@app.route("/remover_aluno/<int:id>", methods=["POST", "GET"])
 def remover_aluno(id):
-     aluno = session.query(Aluno).filter_by(id=id).first() 
-     
-     try:
-          session.delete(aluno)
-          session.commit()
-          return redirect(url_for("listar_alunos"))
-     except:
-             print("erro ao deletar nível")
-     return redirect(url_for('listar_alunos'))
+    aluno = session.query(Aluno).filter_by(id=id).first()
+    
+    try:
+        session.delete(aluno)
+        session.commit()
+        mensagem = "Aluno removido com sucesso."
+    except:
+        session.rollback()
+        mensagem = "Erro ao remover o aluno."
+    
+    return redirect(url_for('listar_alunos', mensagem=mensagem))
 
 @app.route('/editar/<int:id>', methods=['GET', 'POST'])
 def editar_aluno(id):
@@ -110,19 +105,16 @@ def editar_aluno(id):
 
         try:
             session.commit()
-            mensagem = "Dados atualizados com sucesso"
+            mensagem = "Dados atualizados com sucesso."
         except:
             session.rollback()
-            mensagem = "Erro ao atualizar dados"
+            mensagem = "Erro ao atualizar dados."
         finally:
             session.close()
 
         return redirect(url_for('listar_alunos', mensagem=mensagem))
 
     return render_template('editar_aluno.html', aluno=aluno)
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
